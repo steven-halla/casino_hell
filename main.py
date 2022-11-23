a2dd5dd8796ec0862c7d674ae75db0e23d0caec0
steven-halla <69405488+steven-halla@users.noreply.github.com>
2022-11-22 17:52:17 -0800

    "first"

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 import pygame
-import os
-import time
-import random
 
+
+pygame.init()
+
+
+display = pygame.display.set_mode((800, 600))
+clock = pygame.time.Clock()
+
+while True:
+    display.fill((0,0,0))
+    clock.tick(60)
+    pygame.display.update()
