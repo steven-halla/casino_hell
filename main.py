992e3a77f8aac2da32bee4ec8b3cdd06105aecb8
steven-halla <69405488+steven-halla@users.noreply.github.com>
2022-11-22 20:58:35 -0800

    "first"

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+import sys
 
 
 pygame.init()
@@ -7,7 +8,26 @@ pygame.init()
 display = pygame.display.set_mode((800, 600))
 clock = pygame.time.Clock()
 
+
+class Player:
+    def __init__(self, x, y, width, height):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+    def main(self, display):
+        pygame.draw.rect(display, (255, 0, 0 ), (self.x, self.y, self.width, self.hegiht))
+
+player = Player(400, 300, 32, 32)
+
 while True:
     display.fill((0,0,0))
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
+            pygame.QUIT
+
+
     clock.tick(60)
     pygame.display.update()
