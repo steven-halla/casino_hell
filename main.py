0e94dfcf3d70fbbe67d83b5e4d152699aa4ce7c3
steven-halla <69405488+steven-halla@users.noreply.github.com>
2022-11-29 17:32:11 -0800

    "first"

--- main.py
+++ main.py
@@ -16,19 +16,32 @@ class Player:
         self.width = width
         self.height = height
     def main(self, display):
-        pygame.draw.rect(display, (255, 0, 0 ), (self.x, self.y, self.width, self.hegiht))
+        pygame.draw.rect(display, (255, 0, 0 ), (self.x, self.y, self.width, self.height))
 
 player = Player(400, 300, 32, 32)
 
+display_scroll = [0,0]
+
 while True:
-    display.fill((0,0,0))
+    display.fill((24,164,86))
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
             pygame.QUIT
 
-    player.main()
+    keys = pygame.key.get_pressed()
+
+    if keys[pygame.K_a]:
+        display_scroll[0] -= 5
+    if keys[pygame.K_d]:
+        display_scroll[0] += 5
+    if keys[pygame.K_w]:
+        display_scroll[1] -= 5
+    if keys[pygame.K_s]:
+        display_scroll[1] += 5
+
+    player.main(display)
 
 
     clock.tick(60)
