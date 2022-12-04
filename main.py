2d9666e154a88533f1f86e6066b397037879486b
steven-halla <69405488+steven-halla@users.noreply.github.com>
2022-12-03 17:36:03 -0800

    "bare min to move on screen"

--- main.py
+++ main.py
@@ -52,25 +52,7 @@ class Player:
 
 
 
-        # pygame.draw.rect(display, (255, 0, 0 ), (self.x, self.y, self.width, self.height))
 
-
-class PlayerBullet:
-    def __init__(self,x,y,mouse_x, mouse_y):
-        self.x = x
-        self.y = y
-        self.mouse_x = mouse_x
-        self.mouse_y = mouse_y
-        self.speed = 15
-        self.angle = math.atan2(y - mouse_y, x - mouse_x)
-        self.x_vel = math.cos(self.angle) * self.speed
-        self.y_vel = math.sin (self.angle) * self.speed
-
-    def main(self, display):
-        self.x -= int(self.x_vel)
-        self.y -= int(self.y_vel)
-
-        pygame.draw.circle(display, (0,0,0), (self.x, self.y), 5)
 
 class SlimeEnemy:
     def __init__(self, x, y):
@@ -89,85 +71,60 @@ class SlimeEnemy:
             self.animation_count = 0
         self.animation_count += 1
 
-        if self.reset_offset == 0:
-            self.offset_x = random.randrange(-300, 300)
-            self.offset_y = random.randrange(-300, 300)
-            self.reset_offset = random.randrange(120, 150)
-        else:
-            self.reset_offset -= 1
 
-        if player.x + self.offset_x > self.x - display_scroll[0]:
-            self.x += 1
-        elif player.x + self.offset_x > self.x - display_scroll[0]:
-            self.x -= 1
-
-        if player.y + self.offset_y > self.y - display_scroll[1]:
-            self.y += 1
-        elif player.y + self.offset_y > self.y - display_scroll[1]:
-            self.y -= 1
 
         display.blit(pygame.transform.scale(self.animation_images[self.animation_count//4], (32, 30)), (self.x - display_scroll[0], self.y - display_scroll[1]))
 
 
 
-enemies = [SlimeEnemy(400, 300)]
+enemies = [SlimeEnemy(500, 400)]
 
 player = Player(400, 300, 32, 32)
 
 display_scroll = [0,0]
 
-player_bullets = []
+
 
 while True:
     display.fill((24,164,86))
 
-    mouse_x, mouse_y = pygame.mouse.get_pos()
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
             pygame.QUIT
 
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            if event.button == 1:
-                player_bullets.append(PlayerBullet(player.x, player.y, mouse_x, mouse_y ))
+
 
     keys = pygame.key.get_pressed()
 
-    pygame.draw.rect(display, (255, 255, 255), (100 - display_scroll[0], 100 - display_scroll[1], 16, 16))
 
     if keys[pygame.K_a]:
         display_scroll[0] -= 5
 
         player.moving_left = True
 
-        for bullet in player_bullets:
-            bullet.x += 5
+
 
     if keys[pygame.K_d]:
         display_scroll[0] += 5
 
         player.moving_right = True
 
-        for bullet in player_bullets:
-            bullet.x -= 5
 
     if keys[pygame.K_w]:
         display_scroll[1] -= 5
 
-        for bullet in player_bullets:
-            bullet.y += 5
+
 
     if keys[pygame.K_s]:
         display_scroll[1] += 5
-        for bullet in player_bullets:
-            bullet.y -= 5
+
 
     player.main(display)
 
 
-    for bullet in player_bullets:
-        bullet.main(display)
+
 
     for enemy in enemies:
         enemy.main(display)
