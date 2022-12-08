51ad7f7f2dcdbafb3fde1538afa72c45503a34ab
steven-halla <69405488+steven-halla@users.noreply.github.com>
2022-12-07 21:44:17 -0800

    "screen is now black"

--- main.py
+++ main.py
@@ -1,17 +1,31 @@
+
 import pygame
 
 pygame.init()
 
 windowSize = [500, 500]
-screen = pygame.display.set_mode(windowSize)
+display = pygame.display.set_mode(windowSize)
+pygame.display.set_caption("Shooter")
+Green = (0,255,0)
+Blue = (0,0,255)
+Red = (255, 0, 0)
 
-while True :
-    screen.fill('#000000')
+Class
 
+while True:
+    display.fill((124, 164, 114))
+    pygame.draw.rect(display,Red, [10,10,50,50])
+    pygame.draw.rect(display,Blue, [170,70,50,50])
 
     for event in pygame.event.get():
+
+
+
         if event.type == pygame.QUIT:
-            #quits pygame library
+            # quits pygame library
             pygame.quit()
-            #quiet the program
+
+            # quiet the program
             quit()
+
+    pygame.display.update()
