4a63f27de1fdde4eda4c3f77feb5ef9d10bf0c74
steven-halla <69405488+steven-halla@users.noreply.github.com>
2022-12-18 18:45:46 -0800

    state now properly updated with out text

--- coinflip.py
+++ coinflip.py
@@ -27,6 +27,8 @@ class CoinFlipGame:
             text = self.font.render(
                 f"Starting a new game of Coin Flip with a minimum bet of {self.min_bet} and a maximum bet of {self.max_bet}",
                 True, (255, 255, 255))
+            self.screen.fill((0, 0, 0))
+
             self.screen.blit(text, (10, 10))
             pygame.display.flip()
 
@@ -67,34 +69,37 @@ class CoinFlipGame:
             if not is_play_again:
                 break
     #
-    # def prompt_is_play_again(self) -> bool:
-    #     while True:
-    #         choice = input("Would you like to flip again (yes/y or no/n)? ")
-    #         if choice.lower() in ["yes", "y"]:
-    #             print("Let's go again!")
-    #             return True
-    #         elif choice.lower() in ["no", "n"]:
-    #             print("Thank you for playing!")
-    #             return False
-    #         else:
-    #             text = self.font.render("Wrong input. Please enter 'yes' or 'no'.", True, (255, 255, 255))
-    #             screen.blit(text, (10, 130))
-    #             pygame.display.flip()
-    #
-    # def get_bet(self):
-    #     while True:
-    #         try:
-    #             bet = int(input("How much would you like to bet? "))
-    #             # Check if the bet is within the minimum and maximum limits
-    #             if self.min_bet <= bet <= self.max_bet:
-    #                 return bet
-    #             else:
-    #                 print(f"Please enter a bet between {self.min_bet} and {self.max_bet}")
-    #         except ValueError:
-    #             print("Please enter a valid bet amount")
-    #
-    # def flip_coin(self):
-    #     return "heads" if random.randint(0, 1) == 0 else "tails"
+    def prompt_is_play_again(self) -> bool:
+        while True:
+            choice = input("Would you like to flip again (yes/y or no/n)? ")
+            if choice.lower() in ["yes", "y"]:
+                print("Let's go again!")
+                return True
+            elif choice.lower() in ["no", "n"]:
+                print("Thank you for playing!")
+                return False
+            else:
+                text = self.font.render("Wrong input. Please enter 'yes' or 'no'.", True, (255, 255, 255))
+                screen.blit(text, (10, 130))
+                pygame.display.flip()
+
+    def get_bet(self):
+        while True:
+            try:
+                bet = int(input("How much would you like to bet? "))
+                # Check if the bet is within the minimum and maximum limits
+                if self.min_bet <= bet <= self.max_bet:
+                    return bet
+                else:
+                    print(f"Please enter a bet between {self.min_bet} and {self.max_bet}")
+            except ValueError:
+                print("Please enter a valid bet amount")
+
+    def flip_coin(self):
+        return "heads" if random.randint(0, 1) == 0 else "tails"
+
+    def draw(self,screen):
+        screen.blit(self.text, self.text_rect)
 
 # Create a new CoinFlipGame with a minimum bet of 10 and a maximum bet of 100
 coin_flip_game = CoinFlipGame(50, 1000)
