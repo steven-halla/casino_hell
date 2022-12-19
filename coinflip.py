6e68349eaf33961e2047816511b0e58bfe28d286
steven-halla <69405488+steven-halla@users.noreply.github.com>
2022-12-18 18:14:13 -0800

    updated screen size

--- coinflip.py
+++ coinflip.py
@@ -1,4 +1,6 @@
 import random
+import pygame
+
 
 # allow player to pick heads or tails
 
@@ -9,52 +11,71 @@ class CoinFlipGame:
         self.balance = 0
 
 
+    # Initialize Pygame
+    pygame.init()
+
+    # Set the window size and title
+    screen = pygame.display.set_mode((1100, 600))
+    pygame.display.set_caption("Coin Flip Game")
+
+    # Set the font and font size
+    font = pygame.font.Font(None, 36)
+
     def start_coin_game(self):
         while True:
-            print(
-                f"Starting a new game of Coin Flip with a minimum bet of {self.min_bet} and a maximum bet of {self.max_bet} ")
-    
+            # Display the starting message
+            text = self.font.render(
+                f"Starting a new game of Coin Flip with a minimum bet of {self.min_bet} and a maximum bet of {self.max_bet}",
+                True, (255, 255, 255))
+            self.screen.blit(text, (10, 10))
+            pygame.display.flip()
+
             # Get the player's bet
-            bet = self.get_bet()
+            # bet = self.get_bet()
 
             while True:
                 choice = input("Please pick 'heads' or 'tails': ")
                 if choice.lower() in ["heads", "tails"]:
                     break
                 else:
-                    print("Invalid choice. Please enter 'heads' or 'tails'.")
+                    # Display the error message
+                    text = font.render("Invalid choice. Please enter 'heads' or 'tails'.", True, (255, 255, 255))
+                    screen.blit(text, (10, 50))
+                    pygame.display.flip()
 
-                # Flip the coin
+            # Flip the coin
             result = self.flip_coin()
 
             # Calculate the player's new balance
             self.balance += bet if result == choice else -bet
 
-    
-            # Print the result
-            print(f"The coin landed on {result}! Your new balance is {self.balance}")
-    
+            # Display the result message
+            text = font.render(f"The coin landed on {result}! Your new balance is {self.balance}", True,
+                               (255, 255, 255))
+            screen.blit(text, (10, 90))
+            pygame.display.flip()
+
             # Ask the player if they want to play again
             # play_again = input("Do you want to play again (yes/no)? ").lower()
-    
+
             is_play_again = self.prompt_is_play_again()
             if not is_play_again:
                 break
-                
 
-    def prompt_is_play_again(self) -> bool:
-        while True:
-            choice = input("Would you like to flip again (yes/y or no/n)? ")
-            if choice.lower() in ["yes", "y"]:
-                print("Let's go again!")
-                return True
-            elif choice.lower() in ["no", "n"]:
-                print("Thank you for playing!")
-                return False
-            else:
-                print("wrong input ")
-                
-    
+    # def prompt_is_play_again(self) -> bool:
+    #     while True:
+    #         choice = input("Would you like to flip again (yes/y or no/n)? ")
+    #         if choice.lower() in ["yes", "y"]:
+    #             print("Let's go again!")
+    #             return True
+    #         elif choice.lower() in ["no", "n"]:
+    #             print("Thank you for playing!")
+    #             return False
+    #         else:
+    #             text = self.font.render("Wrong input. Please enter 'yes' or 'no'.", True, (255, 255, 255))
+    #             screen.blit(text, (10, 130))
+    #             pygame.display.flip()
+
     def get_bet(self):
         while True:
             try:
@@ -66,14 +87,12 @@ class CoinFlipGame:
                     print(f"Please enter a bet between {self.min_bet} and {self.max_bet}")
             except ValueError:
                 print("Please enter a valid bet amount")
-    
-    
-    def flip_coin(self):
-        return "heads" if random.randint(0, 1) == 0 else "tails"
 
+    # def flip_coin(self):
+    #     return "heads" if random.randint(0, 1) == 0 else "tails"
 
 # Create a new CoinFlipGame with a minimum bet of 10 and a maximum bet of 100
-# coin_flip_game = CoinFlipGame(50, 1000)
-# 
-# # Play the game
-# coin_flip_game.start_coin_game()
+coin_flip_game = CoinFlipGame(50, 1000)
+
+# Play the game
+coin_flip_game.start_coin_game()
