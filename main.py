bbce7116ede9f08281bba243deca1f156b08c4b1
steven-halla <69405488+steven-halla@users.noreply.github.com>
2022-12-23 01:49:32 -0800

    can now exit game

--- main.py
+++ main.py
@@ -50,6 +50,7 @@ class Rectangle:
         self.y: float = y
         self.width: float = width
         self.height: float = height
+        # self.rect = pygame.Rect(x, y, width, height)
 
     def toTuple(self) -> Tuple[float, float, float, float]:
         return (self.x, self.y, self.width, self.height)
@@ -589,4 +590,39 @@ game = Game()
 game.start()
 
 # CoinFlipScreen().start()
+
+
+#+++++++++++++++++++++++++++this could be usefule later +++++++++++++++++++++++++++++++++++++++++++++++++
+# import pygame
+#
+#
+# def display_yes_no_menu(screen, font):
+#     # Create the Yes and No buttons
+#     yes_button = pygame.Rect(100, 100, 50, 50)
+#     no_button = pygame.Rect(200, 100, 50, 50)
+#
+#     # Render the text for the buttons
+#     yes_text = font.render("Yes", True, (0, 0, 0))
+#     no_text = font.render("No", True, (0, 0, 0))
+#
+#     # Blit the buttons and text to the screen
+#     screen.blit(yes_text, (100, 100))
+#     screen.blit(no_text, (200, 100))
+#     pygame.display.update()
+#
+#     # Wait for the user to click one of the buttons
+#     waiting = True
+#     while waiting:
+#         for event in pygame.event.get():
+#             if event.type == pygame.QUIT:
+#                 pygame.quit()
+#                 sys.exit()
+#
+#             # Check if the user clicked the Yes button
+#             if event.type == pygame.MOUSEBUTTONDOWN and yes_button.collidepoint(event.pos):
+#                 return True
+#
+#             # Check if the user clicked the No button
+#             if event.type == pygame.MOUSEBUTTONDOWN and no_button.collidepoint(event.pos):
+#                 return False
 
