7676fe59d166923b08c49faba6ff602d6f08806e
steven-halla <69405488+steven-halla@users.noreply.github.com>
2022-12-23 22:56:59 -0800

    check result function is now working

--- coinflipscreen.py
+++ coinflipscreen.py
@@ -1,4 +1,5 @@
 import random
+import time
 from typing import *
 
 import pygame
@@ -23,12 +24,14 @@ class NewController:
 
                 if event.key == pygame.K_1:
                     self.is1Pressed = True
+                if event.key == pygame.K_t:
+                    self.isTPressed = True
 
                 elif event.type == pygame.KEYUP:
                     if event.key == pygame.K_1:
                         self.is1Pressed = False
-                    elif event.key == pygame.K_1:
-                        self.is1Pressed = False
+                    elif event.key == pygame.K_t:
+                        self.isTPressed = False
 
 
 class BunnyTimes(NewController):
@@ -43,7 +46,10 @@ class BunnyTimes(NewController):
         pygame.display.set_caption("Russian Roulette")
         self.font = pygame.font.Font(None, 36)
         self.game_state = "welcome_opposum"
-        self.winner_or_looser: List[str] = ["lose", "win", "win"]  # List to store the loaded bullets
+        self.winner_or_looser: List[str] = ["lose", "win", "win"]
+        self.result = "win"
+        self.bet = 10
+
 
     def start(self):
         running = True
@@ -63,15 +69,49 @@ class BunnyTimes(NewController):
         print(str(self.winner_or_looser))
         return self.winner_or_looser
 
+    def check_results(self):
+        print("checking result")
+        if self.result == "win":
+            self.bet = self.bet * 2
+            print("you win")
+            print(self.bet)
+
+        elif self.result == "lose":
+            print("you lose")
+
+
 
     def update(self):
+        self.keyPress(self.game_state)
         if self.game_state == "welcome_opposum":
-            print("yo")
+            if self.isTPressed:
+                self.game_state = "choose_can"
+
+        elif self.game_state == "choose_can":
+            if self.is1Pressed:
+                self.shuffle_opposums()
+                self.result = self.winner_or_looser[0]
+                del self.winner_or_looser[0]
+                self.check_results()
+                self.game_state = "play_again_or_bail"
+
+
+
+
+
+
+
+
+
     def draw(self):
         DISPLAY.fill((0,0,0))
 
         if self.game_state == "welcome_opposum":
-            DISPLAY.blit(self.font.render(f"Press J to play again or L to quit", True, (255, 255, 255)), (10, 10))
+            DISPLAY.blit(self.font.render(f"press T", True, (255, 255, 255)), (10, 10))
+        elif self.game_state == "choose_can":
+            DISPLAY.blit(self.font.render(f"Press 1", True, (255, 255, 255)), (10, 10))
+        elif self.game_state == "play_again_or_bail":
+            DISPLAY.blit(self.font.render(f"Press C to continue, or E to exit", True, (255, 255, 255)), (10, 10))
 
 
 
