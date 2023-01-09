21deeb6beb5e6e74c9c477b85ac1de9133315c94
steven-halla <69405488+steven-halla@users.noreply.github.com>
2023-01-09 12:27:52 -0800

    huge bug fixed spells work

--- main.py
+++ main.py
@@ -1173,6 +1173,7 @@ class CoinFlipSandyScreen(Screen):
 
 
 
+
             elif self.result != self.players_side:
                 if self.luck_activated > 0:
                     lucky_draw = random.random()
