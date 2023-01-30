61fe96e6d5ea90fc52eff425f82638ece3247005
steven-halla <69405488+steven-halla@users.noreply.github.com>
2023-01-29 17:50:54 -0800

    now have two methods for messages

--- main.py
+++ main.py
@@ -502,7 +502,7 @@ class CoinFlipExplanationGuy(Npc):
 class InnKeeper(Npc):
     def __init__(self, x: int, y: int):
         super().__init__(x, y)
-        self.textbox = TextBox(
+        self.textbox = NPCTextBox(
             ["1st message.", "2nd message ?", " 3rd message", "4th message"],
             (50, 450, 50, 45), 30, 500)
         self.state_start_time = pygame.time.get_ticks()  # initialize start_time to the current time
@@ -4309,6 +4309,51 @@ class Deck:
 
 
 class TextBox(Entity):
+    def __init__(self, messages: list[str], rect: tuple[int, int, int, int], font_size: int, delay: int, ):
+        super().__init__(rect[0], rect[1], rect[2], rect[3])
+        self.messages = messages
+        self.message_index = 0
+        self.text = self.messages[self.message_index]
+        self.characters_to_display = 0
+        self.font_size = font_size
+        self.delay = delay
+        self.time = pygame.time.get_ticks()
+        self.font = pygame.font.Font(None, 36)
+
+    def update(self, state: "GameState"):
+        controller = state.controller
+
+        # show characters of text one at a time, not whole message.
+        if self.characters_to_display < len(self.text):
+            self.characters_to_display += 1
+
+        # handle button press to see next message
+        if controller.isTPressed and \
+                pygame.time.get_ticks() - self.time > self.delay and \
+                self.message_index < len(self.messages) - 1:
+            pygame.time.delay(700)
+
+            self.time = pygame.time.get_ticks()
+            self.message_index += 1
+            self.text = self.messages[self.message_index]
+            self.characters_to_display = 0
+            state.controller.isTPressed = False
+
+        # print("is finished? " + str(self.is_finished()))
+
+    def draw(self, state: "GameState"):
+        text_to_display = self.text[:self.characters_to_display]
+        wrapped_text = textwrap.wrap(text_to_display, 60)
+        for i, line in enumerate(wrapped_text):
+            text_surface = self.font.render(line, True, (255, 255, 255))
+            DISPLAY.blit(text_surface, (self.position.x, self.position.y + (i * 40)))
+
+    def is_finished(self) -> bool:
+        return self.message_index == len(self.messages) - 1 and \
+            pygame.time.get_ticks() - self.time > self.delay
+
+
+class NPCTextBox(Entity):
     def __init__(self, messages: list[str], rect: tuple[int, int, int, int], font_size: int, delay: int):
         super().__init__(rect[0], rect[1], rect[2], rect[3])
         self.messages = messages
