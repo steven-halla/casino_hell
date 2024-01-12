5f5b275500ecbd5914aae47018aa4a0c72ad0689
steven-halla <steven.r.halla@gmail.com>
2024-01-11 20:37:18 -0800

    can now sort of talk to treasure chests

--- src/entity/treasurechests/treasurechests.py
+++ src/entity/treasurechests/treasurechests.py
@@ -8,15 +8,27 @@ from constants import PURPLE
 from entity.entity import Entity
 
 class TreasureChest(Entity):
-    def __init__(self, x: float, y: float):
+    def __init__(self, x: float, y: float, treasure_item: str):
         super().__init__(x, y, 16, 16)  # Assuming 16x16 is the size of the chest
         self.color: Tuple[int, int, int] = PURPLE
+        self.treasure_item = treasure_item  # The item in the chest
+
 
     def update(self, state: "GameState"):
         # Check for collision with the player
         if self.collision.isOverlap(state.player.collision):
             # Prevent the player from moving through the chest
             state.player.undoLastMove()
+
+        if not self.isOpened and state.controller.isTPressed:
+            # Open the chest and give the item
+            self.open_chest(state)
+
+    def give_item(self, state: "GameState"):
+
+        print(f"Received item: {self.item}")
+        state.player.items.append(self.item)
+        print("Your inventory so far: " + str(state.player.inventory))
 
     def draw(self, state: "GameState"):
         rect = (
--- src/entity/treasurechests/waterbottle.py
+++ src/entity/treasurechests/waterbottle.py
@@ -1,9 +1,36 @@
+import math
+
 from entity.treasurechests.treasurechests import TreasureChest
 import pygame
 
 
 class WaterBottle(TreasureChest):
-    pass
+    def __init__(self, x: float, y: float):
+        # Pass the hidden item to the superclass constructor
+        super().__init__(x, y, "Water Bottle")
+        self.hidden_item = "Water Bottle"
+        self.isOpened = False  # Define and initialize 'isOpened' attribute
+        self.state_start_time = pygame.time.get_ticks()  # initialize start_time to the current time
 
 
+    def give_item(self, state: "GameState"):
+        if state.controller.isTPressed:
+            print("Hi there water")
+            print(f"Received item: {self.hidden_item}")
+            state.player.items.append(self.hidden_item)
+            print("Your inventory so far: " + str(state.player.items))
+            state.treasure_chests.remove(self)  # Remove the chest from the game
+            self.isOpened = True  #
+
+    def open_chest(self, state: "GameState"):
+        # Implement the logic for opening the chest here
+        if state.controller.isTPressed and (
+                pygame.time.get_ticks() - self.state_start_time) > 500:
+            distance = math.sqrt(
+                (state.player.collision.x - self.collision.x) ** 2 + (
+                        state.player.collision.y - self.collision.y) ** 2)
+            # print("distance: " + str(distance))
+
+            if distance < 40:
+                print("Yo ho ho and a bottle of bum")
 
