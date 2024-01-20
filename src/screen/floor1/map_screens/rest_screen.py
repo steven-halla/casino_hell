df20d21e0dde628999570c83905c895a3284e8b5
steven-halla <steven.r.halla@gmail.com>
2024-01-20 14:07:53 -0800

    went a new route to append NPC with a new NPC

--- src/screen/floor1/map_screens/rest_screen.py
+++ src/screen/floor1/map_screens/rest_screen.py
@@ -7,6 +7,7 @@ from entity.npc.hedge_maze_screen.hedgehog2 import HedgeHog2
 from entity.npc.hedge_maze_screen.hedgehog3 import HedgeHog3
 from entity.npc.hedge_maze_screen.hedgehog4 import HedgeHog4
 from entity.npc.rest_screen.bar_keep import BarKeep
+from entity.npc.rest_screen.bar_keep_low_body import BarKeepLowBody
 from entity.npc.rest_screen.doctor_opossum import DoctorOpossum
 from entity.npc.rest_screen.inn_keeper import InnKeeper
 from entity.npc.rest_screen.justin_no_fruit import JustinNoFruit
@@ -48,14 +49,16 @@ class RestScreen(Screen):
             # WaterBottle(16 * 36, 16 * 10),
 
         ]
+        # Check the value of state.player.body
 
         # state.npcs = []
-        state.npcs = [
-            # make sure to seperate by a factor of 8 for y
-            #x, y
-            # InnGuard(16 * 36, 16 * 2),
-            # BappingMike(16 * 36, 16 * 10),
-            BarKeep(16 * 36, 16 * 18),
+        if state.player.body > 0:
+            state.npcs.append(BarKeep(16 * 36, 16 * 18))
+        elif state.player.body == 0:
+            state.npcs.append(BarKeepLowBody(16 * 36, 16 * 18))
+
+        # Add other NPCs to the state.npcs list
+        state.npcs.extend([
             DoctorOpossum(16 * 26, 16 * 18),
             InnKeeper(16 * 18, 16 * 18),
             JustinNoFruit(16 * 10, 16 * 18),
@@ -63,50 +66,7 @@ class RestScreen(Screen):
             ShopKeeper(16 * 18, 16 * 26),
             SufferingSuzy(16 * 26, 16 * 26),
             WallyGuide(16 * 34, 16 * 26),
-            # InnKeeper(16 * 36, 16 * 26),
-            # DoctorOpossum(16 * 30, 16 * 30),
-            # JackyBanana(16* 5, 16 * 15),
-            # BappingMike(16* 15, 16 * 15),
-            # HungryPatrick(16* 25, 16 * 15),
-            # InnGuard(16* 35, 16 * 15),
-            # NickyHints(16* 25, 16 * 25),
-            # MainScreenTeleporter(16 * 1, 16 * 10),
-            #
-            #
-            #
-            # FlippinTed(16* 35, 16 * 34),
-
-
-            # BobbyBibs(16 * 2, 16 * 2),
-            # BrutalPatrick(16 * 2, 16 * 10),
-            # ChillyBilly(16 * 2, 16 * 18),
-            #  CindyLongHair(16 * 2, 16 * 26),
-             # HungryPatrick(16 * 2, 16 * 34),
-            # JackyBanana(16 * 10, 16 * 2),
-            # JustinNoFruit(16 * 10, 16 * 10),
-            # NickyHints(16 * 10, 16 * 18),
-            # QuestGiverJanet(16 * 10, 16 * 26),
-            # RumbleBill(16 * 18, 16 * 2),
-            # SallyOpossum(16 * 18, 16 * 10),
-            # ShopKeeper(16 * 18, 16 * 18),
-            # SleepyNed(16 * 18, 16 * 26),
-            # SufferingSuzy(16 * 26, 16 * 2),
-            # WallyGuide(16 * 26, 16 * 10),
-            #
-            # CoinFlipFred(16 * 28, 16 * 36),
-            # FlippinTed(16 * 20, 16 * 36),
-            # FlippingSandy(16 * 28, 16 * 36),
-            # NellyOpossum(16 * 12, 16 * 36),
-            # Nurgle(16 * 24, 16 * 34)
-            # HedgeHog1(16 * 24, 16 * 34),
-            # HedgeHog2(16 * 32, 16 * 34),
-            # HedgeHog3(16 * 24, 16 * 20),
-            # HedgeHog4(16 * 24, 16 * 15),
-            # SirLeopoldTheHedgeHog(16 * 24, 16 * 25),
-
-
-
-                      ]
+        ])
         #
         # if state.quest_giver_janet.find_hog:
         #     state.npcs.append(Nurgle(16 * 24, 16 * 34))
@@ -132,6 +92,8 @@ class RestScreen(Screen):
             npc.update(state)
             if isinstance(npc, Nurgle) and npc.to_be_deleted:
                 state.npcs.remove(npc)
+
+
 
         # Assuming you have your hedgehog instances named like HedgeHog1, HedgeHog2, etc.
         # hedgehogs = [HedgeHog1(), HedgeHog2(), HedgeHog3(), HedgeHog4()]
@@ -224,66 +186,7 @@ class RestScreen(Screen):
         state.camera.x = PLAYER_OFFSET[0] - state.player.collision.x
         state.camera.y = PLAYER_OFFSET[1] - state.player.collision.y
 
-    # def draw(self, state: "GameState"):
-    #     state.DISPLAY.fill(BLUEBLACK)
-    #     state.DISPLAY.blit(state.FONT.render(
-    #         f"player money: {state.player.money}",
-    #         True, (255, 255, 255)), (333, 333))
-    #     state.DISPLAY.blit(state.FONT.render(
-    #         f"player stamina points: {state.player.stamina_points}",
-    #         True, (255, 255, 255)), (333, 388))
-    #
-    #     if self.tiled_map.layers:
-    #         tile_width = self.tiled_map.tilewidth
-    #         tile_height = self.tiled_map.tileheight
-    #
-    #         # Get the background layer
-    #         bg_layer = self.tiled_map.get_layer_by_name("bg")
-    #         # Iterate over the tiles in the background layer
-    #         for x, y, image in bg_layer.tiles():
-    #             # Calculate the position of the tile in pixels
-    #             pos_x = x * tile_width + state.camera.x
-    #             pos_y = y * tile_height + state.camera.y
-    #
-    #             scaled_image = pygame.transform.scale(image, (
-    #                 tile_width * 1.3, tile_height * 1.3))
-    #
-    #             state.DISPLAY.blit(scaled_image, (pos_x, pos_y))
-    #
-    #         # Get the collision layer
-    #         collision_layer = self.tiled_map.get_layer_by_name("collision")
-    #         for x, y, image in collision_layer.tiles():
-    #             # Calculate the position of the tile in pixels
-    #             pos_x = x * tile_width + state.camera.x
-    #             pos_y = y * tile_height + state.camera.y
-    #
-    #             scaled_image = pygame.transform.scale(image, (
-    #                 tile_width * 1.3, tile_height * 1.3))
-    #
-    #             state.DISPLAY.blit(scaled_image, (pos_x, pos_y))
-    #
-    #     for npc in state.npcs:
-    #         npc.draw(state)
-    #         if isinstance(npc, WallyGuide):
-    #             # Assuming npc.collision has x, y, width, and height attributes
-    #             rect = (npc.collision.x, npc.collision.y, npc.collision.width, npc.collision.height)
-    #             pygame.draw.rect(state.DISPLAY, (0, 255, 0), rect, 2)
-    #
-    #     for demon in state.demons:
-    #         demon.draw(state)
-    #
-    #     for treasurechests in state.treasurechests:
-    #         treasurechests.draw(state)
-    #
-    #     state.obstacle.draw(state)
-    #
-    #     state.player.draw(state)
-    #
-    #     # Draw Player's collision box in red
-    #     pygame.draw.rect(state.DISPLAY, (255, 0, 0), state.player.collision.toTuple(), 2)
-    #
-    #     # ... (rest of your drawing code, like updating the display) ...
-    #     pygame.display.update()
+
 
     def draw(self, state: "GameState"):
         state.DISPLAY.fill(BLUEBLACK)
