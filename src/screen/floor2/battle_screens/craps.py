f57fbfef8d7fc6ee7b76de8f99f298c5f752e7c0
steven-halla <steven.r.halla@gmail.com>
2024-07-01 19:57:53 -0700

    restored

--- src/screen/floor2/battle_screens/craps.py
+++ src/screen/floor2/battle_screens/craps.py
@@ -14,7 +14,7 @@ class Craps(BattleScreen):
         self.game_state: str = "welcome_screen"
         self.battle_messages: dict[str, TextBox] = {
             "welcome_message": TextBox(
-                [" "],
+                ["Time to take a crap with some craps"],
                 (65, 460, 700, 130),
                 36,
                 500
@@ -115,9 +115,9 @@ class Craps(BattleScreen):
 
             if self.welcome_screen_index == 0 and controller.isTPressed:
                 self.game_state = "spin_screen"
-                state.player.stamina_points -= 4
-                state.player.money -= self.bet
-                self.money += self.bet
+                # state.player.stamina_points -= 4
+                # state.player.money -= self.bet
+                # self.money += self.bet
                 controller.isTPressed = False
             elif self.welcome_screen_index == 1 and controller.isTPressed and self.magic_lock == False:
                 self.magic_screen_index = 0
@@ -154,21 +154,147 @@ class Craps(BattleScreen):
 
 
 
-
-
-
-
-
-
-
-
-
     def draw(self, state: "GameState") -> None:
         state.DISPLAY.fill((0, 0, 51))
         self.draw_hero_info_boxes(state)
         self.draw_enemy_info_box(state)
 
         self.draw_bottom_black_box(state)
+
+        if self.game_state == "welcome_screen":
+            self.battle_messages["welcome_message"].draw(state)
+
+            if self.money < 1:
+                self.battle_messages["you_win"].draw(state)
+
+            if state.player.stamina_points < 1:
+                self.battle_messages["game_over_no_stamina_message"].draw(state)
+
+            elif state.player.money <= 0:
+                self.battle_messages["game_over_no_money_message"].draw(state)
+
+
+            elif state.player.stamina_points <= 10 and state.player.stamina_points > 0:
+                self.battle_messages["game_over_low_stamina_message"].draw(state)
+
+
+
+            elif state.player.money < 50 and state.player.money > 0:
+                self.battle_messages["game_over_low_money_message"].draw(state)
+
+
+            black_box_height = 221 - 50  # Adjust height
+            black_box_width = 200 - 10  # Adjust width to match the left box
+            border_width = 5
+            start_x_right_box = state.DISPLAY.get_width() - black_box_width - 25
+            start_y_right_box = 240  # Adjust vertical alignment
+
+            # Create the black box
+            black_box = pygame.Surface((black_box_width, black_box_height))
+            black_box.fill((0, 0, 0))
+
+            # Create a white border
+            white_border = pygame.Surface(
+                (black_box_width + 2 * border_width, black_box_height + 2 * border_width)
+            )
+            white_border.fill((255, 255, 255))
+            white_border.blit(black_box, (border_width, border_width))
+
+            # Determine the position of the white-bordered box
+            black_box_x = start_x_right_box - border_width
+            black_box_y = start_y_right_box - border_width
+
+            # Blit the white-bordered box onto the display
+            state.DISPLAY.blit(white_border, (black_box_x, black_box_y))
+
+            # Draw the menu options
+            for idx, choice in enumerate(self.welcome_screen_choices):
+                y_position = start_y_right_box + idx * 40  # Adjust spacing between choices
+                state.DISPLAY.blit(
+                    self.font.render(choice, True, (255, 255, 255)),
+                    (start_x_right_box + 60, y_position + 15)
+                )
+            if "Hack" not in state.player.magicinventory:
+                self.magic_lock = True
+                self.welcome_screen_choices[1] = "Locked"
+
+            if "Lucky Shoes" in state.player.items:
+                self.welcome_screen_choices[2] = "Locked"
+
+            if self.lock_down > 0:
+
+                self.welcome_screen_choices[3] = "Locked"
+
+            if self.welcome_screen_index == 0:
+                state.DISPLAY.blit(
+                    self.font.render("->", True, (255, 255, 255)),
+                    (start_x_right_box + 12, start_y_right_box + 12)
+                )
+            elif self.welcome_screen_index == 1:
+                state.DISPLAY.blit(
+                    self.font.render("->", True, (255, 255, 255)),
+                    (start_x_right_box + 12, start_y_right_box + 52)
+                )
+            elif self.welcome_screen_index == 2:
+                state.DISPLAY.blit(
+                    self.font.render("->", True, (255, 255, 255)),
+                    (start_x_right_box + 12, start_y_right_box + 92)
+                )
+            elif self.welcome_screen_index == 3:
+                state.DISPLAY.blit(
+                    self.font.render("->", True, (255, 255, 255)),
+                    (start_x_right_box + 12, start_y_right_box + 132)
+                )
+
+        elif self.game_state == "magic_screen":
+            self.battle_messages["magic_message"].draw(state)
+
+            black_box_height = 221 - 50  # Adjust height
+            black_box_width = 200 - 10  # Adjust width to match the left box
+            border_width = 5
+            start_x_right_box = state.DISPLAY.get_width() - black_box_width - 25
+            start_y_right_box = 240  # Adjust vertical alignment
+
+            # Create the black box
+            black_box = pygame.Surface((black_box_width, black_box_height))
+            black_box.fill((0, 0, 0))
+
+            # Create a white border
+            white_border = pygame.Surface(
+                (black_box_width + 2 * border_width, black_box_height + 2 * border_width)
+            )
+            white_border.fill((255, 255, 255))
+            white_border.blit(black_box, (border_width, border_width))
+
+            # Determine the position of the white-bordered box
+            black_box_x = start_x_right_box - border_width
+            black_box_y = start_y_right_box - border_width
+
+            # Blit the white-bordered box onto the display
+            state.DISPLAY.blit(white_border, (black_box_x, black_box_y))
+
+            # Draw the menu options
+            for idx, choice in enumerate(self.magic_screen_choices):
+                y_position = start_y_right_box + idx * 40  # Adjust spacing between choices
+                state.DISPLAY.blit(
+                    self.font.render(choice, True, (255, 255, 255)),
+                    (start_x_right_box + 60, y_position + 15)
+                )
+
+            if self.magic_screen_index == 0:
+                state.DISPLAY.blit(
+                    self.font.render("->", True, (255, 255, 255)),
+                    (start_x_right_box + 12, start_y_right_box + 12)
+                )
+            elif self.magic_screen_index == 1:
+                state.DISPLAY.blit(
+                    self.font.render("->", True, (255, 255, 255)),
+                    (start_x_right_box + 12, start_y_right_box + 52)
+                )
+
+        elif self.game_state == "bet_screen":
+            self.battle_messages["bet_message"].draw(state)
+
         pygame.display.flip()
 
 
