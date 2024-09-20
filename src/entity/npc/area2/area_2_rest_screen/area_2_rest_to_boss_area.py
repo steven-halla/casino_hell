69fb86099af8d48856d5a5703e3bb144174c6bfe
steven-halla <steven.r.halla@gmail.com>
2024-09-20 15:47:04 -0700

    fixed boss door which now requires a key

--- src/entity/npc/area2/area_2_rest_screen/area_2_rest_to_boss_area.py
+++ src/entity/npc/area2/area_2_rest_screen/area_2_rest_to_boss_area.py
@@ -9,7 +9,7 @@ class Area2RestToBossArea(Npc):
         self.selected_item_index = 0
         self.flipping_ted_messages = {
             "welcome_message": NpcTextBox(
-                ["Gambling Guard: You need to get prove yourself first. Go take down Ted, that ugly rat faced bastard has it coming.`"],
+                ["You need a key"],
                 (50, 450, 700, 130), 36, 500),
             "defeated_message": NpcTextBox(
                 ["Going to Gambling area"],
@@ -33,30 +33,52 @@ class Area2RestToBossArea(Npc):
         if self.state == "waiting":
             self.update_waiting(state)
         elif self.state == "talking":
-            self.update_talking(state)
+            current_message = self.flipping_ted_messages["welcome_message"]
+
+            if current_message.message_index == 1:
+                if state.controller.isAPressed and pygame.time.get_ticks() - self.input_time > 500:
+                    self.input_time = pygame.time.get_ticks()
+                    self.state = "waiting"
+
+
+                elif state.controller.isBPressed and pygame.time.get_ticks() - self.input_time > 500:
+                    self.input_time = pygame.time.get_ticks()
+                    self.state = "waiting"
+
+            self.update_talking(state, current_message)
 
     def update_waiting(self, state: "GameState"):
         player = state.player
-        distance = math.sqrt((player.collision.x - self.collision.x) ** 2 +
-                             (player.collision.y - self.collision.y) ** 2)
+        min_distance = math.sqrt((player.collision.x - self.collision.x) ** 2 + (player.collision.y - self.collision.y) ** 2)
 
-        if distance < 40 and state.controller.isTPressed and \
-                (pygame.time.get_ticks() - self.state_start_time) > 500 and state.player.menu_paused == False:
+        if min_distance < 10:
+            print("nooo")
 
-            self.state_start_time = pygame.time.get_ticks()
-            if state.controller.isTPressed:
-                state.area_2_rest_area_to_boss_point = True
+        if state.controller.isTPressed and (pygame.time.get_ticks() - self.state_start_time) > 500:
+            distance = math.sqrt((player.collision.x - self.collision.x) ** 2 + (player.collision.y - self.collision.y) ** 2)
 
-                state.currentScreen = state.area2BossScreen
-                state.area2BossScreen.start(state)
-            # Reset the message depending on the game state
+            if distance < 40 and state.player.menu_paused == False:
+                self.state = "talking"
+                self.state_start_time = pygame.time.get_ticks()
+                # Reset the message based on player state
+                current_message = self.flipping_ted_messages["welcome_message"]
 
-    def update_talking(self, state: "GameState"):
-        current_message = self.flipping_ted_messages["welcome_message"]
+                current_message.reset()
+
+    def update_talking(self, state: "GameState", current_message):
         current_message.update(state)
-
-        # Lock the player in place while talking
         state.player.canMove = False
+        if "boss key" in state.player.quest_items:
+            state.area_2_rest_area_to_boss_point = True
+
+            state.currentScreen = state.area2BossScreen
+            state.area2BossScreen.start(state)
+
+        if state.controller.isTPressed and current_message.is_finished():
+            self.state = "waiting"
+            self.state_start_time = pygame.time.get_ticks()
+            state.player.canMove = True
+
 
 
 
@@ -64,7 +86,7 @@ class Area2RestToBossArea(Npc):
 
     def draw(self, state):
 
+        if self.state == "talking" and "boss key" not in state.player.quest_items:
+            current_message = self.flipping_ted_messages["welcome_message"]
 
-        if self.state == "talking":
-            current_message = self.flipping_ted_messages["defeated_message"] if state.coinFlipTedScreen.coinFlipTedDefeated else self.flipping_ted_messages["welcome_message"]
             current_message.draw(state)
