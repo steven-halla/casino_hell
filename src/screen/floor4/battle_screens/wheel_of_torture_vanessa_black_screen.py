2f550ea17c40b00a26b438d32c7602bd7a46f5fb
steven-halla <steven.r.halla@gmail.com>
2025-04-08 12:54:53 -0700

    player/enemy tokens created alogn with other methods

--- src/screen/floor4/battle_screens/wheel_of_torture_vanessa_black_screen.py
+++ src/screen/floor4/battle_screens/wheel_of_torture_vanessa_black_screen.py
@@ -1,11 +1,15 @@
-from pygame import surface
+from typing import List
 
-from constants import BLACK, RED
+from pygame import surface
+from typeguard import typechecked
+
+from constants import BLACK, RED, PURPLE, GREEN
 from entity.gui.screen.gamble_screen import GambleScreen
 from entity.gui.textbox.message_box import MessageBox
 from entity.npc.npc import Npc
 import pygame
 from game_constants.events import Events
+import random
 
 
 
@@ -24,6 +28,12 @@ class WheelOfTortureVanessaBlackScreen(GambleScreen):
         self.magic_menu_index: int = 0
         self.spirit_bonus: int = 0
         self.magic_bonus: int = 0
+        self.move_player: int = 0
+        self.move_dealer: int = 0
+        self.player_position: int = 0
+        self.enemy_position: int = 0
+        self.board_squares: List[str] = []
+
 
         self.battle_messages: dict[str, MessageBox] = {
             self.WELCOME_SCREEN: MessageBox([
@@ -54,19 +64,71 @@ class WheelOfTortureVanessaBlackScreen(GambleScreen):
             state.currentScreen = state.area4RestScreen
             state.area4RestScreen.start(state)
             Events.add_level_four_event_to_player(state.player, Events.WHEEL_OF_TORTURE_VANESSA_BLACK_DEFEATED)
-
         if self.game_state == self.WELCOME_SCREEN:
             pass
 
     def draw(self, state):
         super().draw(state)
-
         if self.game_state == self.WELCOME_SCREEN:
             self.draw_board(state)
-
-
+            self.draw_enemy_token(state)
+            self.draw_player_token(state)
         pygame.display.flip()
 
+#============================================update methods go below
+
+    @typechecked
+    def update_square_effects(self) -> None:
+        current_player_square = self.board_squares[self.player_position]
+        current_enemy_square = self.board_squares[self.enemy_position]
+
+    @typechecked
+    def update_board(self) -> None:
+        """Initializes the board as a list of 30 squares."""
+        self.board_squares: list = [None] * 30
+
+
+    @typechecked
+    def update_roll_dice_player(self) -> None:
+        self.move_player = random.randint(1, 6)
+
+
+    @typechecked
+    def update_roll_dice_dealer(self) -> None:
+        self.move_dealer = random.randint(1, 6)
+
+
+
+
+#_-----------------------------------draw methods go below
+
+    @typechecked
+    def draw_player_token(self, state) -> None:
+        """Draws the player's token (PURPLE) in square 1."""
+        token_size: int = 15
+        x_start: int = 50
+        y_position: int = 100
+        padding_inside_square: int = 5
+
+        token_x: int = x_start + padding_inside_square
+        token_y: int = y_position + padding_inside_square
+
+        pygame.draw.rect(state.DISPLAY, PURPLE, (token_x, token_y, token_size, token_size))
+
+    @typechecked
+    def draw_enemy_token(self, state) -> None:
+        """Draws the enemy's token (GREEN) in square 1."""
+        token_size: int = 15
+        x_start: int = 45
+        y_position: int = 95
+        padding_inside_square: int = 35  # shifts token lower-right in the square
+
+        token_x: int = x_start + padding_inside_square
+        token_y: int = y_position + padding_inside_square
+
+        pygame.draw.rect(state.DISPLAY, GREEN, (token_x, token_y, token_size, token_size))
+
+    @typechecked
     def draw_board(self, state) -> None:
         x_start = 50
         y_position = 100
@@ -76,6 +138,7 @@ class WheelOfTortureVanessaBlackScreen(GambleScreen):
         box_width: int = 50
         border_thickness: int = 2
         x_padding: int = 70
+
 
         for i in range(10):
             x_position = x_start + i * x_padding
